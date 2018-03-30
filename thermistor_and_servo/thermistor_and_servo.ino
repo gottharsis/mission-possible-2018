0c47e8c4d1f60d2de4080d6c12806d87af83a74a
gottharsis <ayush3108@gmail.com>
2018-03-30 19:10:26 -0400

    Update thermistor_and_servo.ino
    Add print statements

--- thermistor_and_servo/thermistor_and_servo.ino
+++ thermistor_and_servo/thermistor_and_servo.ino
@@ -8,13 +8,16 @@ void setup() {
   // put your setup code here, to run once:
   servo.attach(2); //attach Servo to Digital Output 2;
   servo.write(90);
+  Serial.begin(9600);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //attach input to analog pin 0
-  if (!actionComplete && analogRead(0) <= THRESHOLD) {
+  int reading = analogRead(0);
+  Serial.println(reading);
+  if (!actionComplete &&  reading <= THRESHOLD) {
     servo.write(0); //change to 180 for opposite direction
     actionComplete = true; //only triggers once.
   }
